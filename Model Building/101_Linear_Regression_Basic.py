c75275aabd14386ac1a615afbacca96677d88293
Navid Hejazi <navid.hejazi@Tensator.com>
2025-02-23 16:07:04 +0000

    Linear regression basic

---
+++ Model Building/101_Linear_Regression_Basic.py
@@ -0,0 +1,50 @@
+################################################
+# Linear Regression - Basic Template
+################################################
+
+
+# Import required python package
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import pandas as pd
+
+
+
+# Import sample data
+
+my_df = pd.read_csv("data/sample_data_regression.csv")
+
+
+# split data into input and output objects
+
+X = my_df.drop(["output"], axis = 1)
+y = my_df["output"]
+
+
+# split data into training and test sets
+
+X_train, X_test, y_train, y_test = train_test_split(X , y, test_size = 0.2, random_state = 42)
+
+
+# Instantiate our model object
+
+regressor = LinearRegression()
+
+
+# Train our model
+
+regressor.fit(X_train, y_train)
+
+
+# Asess model accuracy
+
+y_pred = regressor.predict(X_test)
+
+r2_score(y_test, y_pred) #  0.7520950550848781
+
+
+
+
+
